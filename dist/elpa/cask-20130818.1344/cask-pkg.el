6de9cb868d582cea1ffc117bb2793a04f776d187
Maciej Katafiasz <mathrick@gmail.com>
2013-08-28 18:10:53 +0200

    No longer track dist/

--- dist/elpa/cask-20130818.1344/cask-pkg.el
+++
@@ -1,2 +0,0 @@
-(define-package "cask" "20130818.1344" "Emacs dependency management made easy [github]" 'nil)
-
--- dist/elpa/pallet-20130827.2107/pallet-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "pallet" "20130827.2107" "A package manager for Emacs, using Cask. [github]"
-  '((cask "0.4.6")))
-
