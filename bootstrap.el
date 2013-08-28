8a1759e964ec00604ac8820bfae03f801ac5a2d0
Maciej Katafiasz <mathrick@gmail.com>
2013-08-28 17:08:01 +0200

    A bunch of fixes to bootstrap.el

--- bootstrap.el
+++ bootstrap.el
@@ -7,20 +7,22 @@
 ;; 0. Open Emacs. Most config won't work, but that's OK, as grail
 ;;    suppresses errors
 ;; 1. Switch to *scratch*
-;; 2. (grail-install-elpa)
-;; 3. C-j
 ;; 2. (load-file "~/elisp/bootstrap.el")
 ;; 3. C-j
-
 ;; 4. When it's done, there should be a working config. Restart Emacs,
 ;;    check if no errors are reported in *scratch* (that's where grail
 ;;    stuffs them)
 
 (defun bootstrap-elisp ()
+  (grail-install-elpa)
   (save-excursion
     (switch-to-buffer (url-retrieve-synchronously
 		       "https://raw.github.com/milkypostman/melpa/master/melpa.el"))
     (package-install-from-buffer (package-buffer-info) 'single)
-    (package-install "pallet"))
+    (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+    (when (< emacs-major-version 24)
+      (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+    (package-refresh-contents)
+    (package-install 'pallet)))
 
 (bootstrap-elisp)
