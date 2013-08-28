fe5309d38a4558b4085a47776f387b91d90a74e5
Maciej Katafiasz <mathrick@gmail.com>
2013-08-28 17:51:08 +0200

    Fix up pallet to avoid calling cask-initialize

--- user.el
+++ user.el
@@ -13,7 +13,18 @@
 (defun pt/cask-file ()
   "Location of the Cask file."
   (expand-file-name "Cask" grail-elisp-root))
-(cask-initialize)
+
+;; We cannot just call (cask-initialize), since it assumes ~/.emacs.d,
+;; and similarly we have to fix up pt/cask-up since it calls that
+(defun pt/cask-up (&optional body)
+  "Attempt to initialize Cask, optionally running BODY."
+  (if (file-exists-p (pt/cask-file))
+      (progn
+	(setq cask-runtime-dependencies '())
+	(cask-setup (file-name-directory (pt/cask-file)))
+	(epl-initialize)
+	(when body (funcall body)))
+    (message "No Cask file found. Run `pallet-init' to create one.")))
 
 (mouse-wheel-mode t)
 (column-number-mode t)
