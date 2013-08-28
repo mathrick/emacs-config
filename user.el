58f30c49f7937d214b2e29301ce189d10c532a02
Maciej Katafiasz <mathrick@gmail.com>
2013-08-28 18:06:49 +0200

    Split out the messy Pallet hacks into a separate file

--- user.el
+++ user.el
@@ -10,21 +10,7 @@
 ;; Pallet takes care of summarising installed packages as a Cask manifest
 (require 'pallet)
 ;; We need to coax it to play nice with grail
-(defun pt/cask-file ()
-  "Location of the Cask file."
-  (expand-file-name "Cask" grail-elisp-root))
-
-;; We cannot just call (cask-initialize), since it assumes ~/.emacs.d,
-;; and similarly we have to fix up pt/cask-up since it calls that
-(defun pt/cask-up (&optional body)
-  "Attempt to initialize Cask, optionally running BODY."
-  (if (file-exists-p (pt/cask-file))
-      (progn
-	(setq cask-runtime-dependencies '())
-	(cask-setup (file-name-directory (pt/cask-file)))
-	(epl-initialize)
-	(when body (funcall body)))
-    (message "No Cask file found. Run `pallet-init' to create one.")))
+(load-library "pallet-overrides")
 
 (mouse-wheel-mode t)
 (column-number-mode t)
