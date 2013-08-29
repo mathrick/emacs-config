19a7c63c0f245950a227d3a0069fe7dca89925cb
Maciej Katafiasz <mathrick@gmail.com>
2013-08-29 19:06:33 +0200

    Cosmetic changes in keys.el

--- keys.el
+++ keys.el
@@ -4,17 +4,17 @@
 (windmove-default-keybindings 'meta)
 
 ;; List and sexp navigation
-(global-set-key [(meta control right)] 'forward-list)
-(global-set-key [(meta control left)] 'backward-list)
-(global-set-key [(meta n)] 'up-list)
-(global-set-key [(control super right)] 'forward-sexp)
-(global-set-key [(control super left)] 'backward-sexp)
+(global-set-key "C-M-<right>" 'forward-list)
+(global-set-key "C-M-<left>"  'backward-list)
+(global-set-key "M-n"         'up-list)
+(global-set-key "C-s-<right>" 'forward-sexp)
+(global-set-key "C-s-<left>"  'backward-sexp)
 
 ;; Misc
-(global-set-key (kbd "C-c k") 'browse-kill-ring)
+(global-set-key (kbd "C-c k")   'browse-kill-ring)
 (global-set-key (kbd "C-c C-v") 'uncomment-region)
-(global-set-key (kbd "C-h d") 'describe-function)
-(global-set-key (kbd "C-h a") 'apropos)
+(global-set-key (kbd "C-h d")   'describe-function)
+(global-set-key (kbd "C-h a")   'apropos)
 
 ;; Text manip and navigation
 (global-set-key (kbd "C-f") 'goto-char-forward)
@@ -47,7 +47,7 @@
 (local-hook-key 'dired-mode-hook "r" 'wdired-change-to-wdired-mode)
 
 ;; GDB
-(local-hook-key 'gud-mode-hook [(control -)] 'gud-attach-dwim)
+(local-hook-key 'gud-mode-hook (kbd "C-c C--") 'gud-attach-dwim)
 
 ;; C
 (local-hook-key 'c-mode-common-hook (kbd "RET")       'newline-and-indent)
