230984519f87ed2dc8c0b3562b79e9c105ab855b
Maciej Katafiasz <mathrick@gmail.com>
2013-08-29 12:06:09 +0200

    So much for lexically scoped Elisp. Use lexical-let in local-hook-key, since 23 doesn't have lexical-binding: t

--- elisp.el
+++ elisp.el
@@ -3,4 +3,7 @@
 
 (defun local-hook-key (hook key command)
   "Instal a hook in HOOK that binds KEY to COMMAND via local-set-key"
-  (add-hook hook (lambda () (local-set-key key command))))
+  (lexical-let ((hook hook)
+                (key key)
+                (command command))
+   (add-hook hook (lambda () (local-set-key key command)))))
